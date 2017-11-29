35c40c18ed22f9b58e94b056b1da32793c144ccc
umarkotak <umarkotak@yahoo.co.id>
2017-11-29 09:25:20 +0700

    6 add view layouts main with integarted assets

---
+++ application/views/layouts/main.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Home</title>
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/bower_components/bootstrap/dist/css/bootstrap.min.css">
+
+  <script src="<?php echo base_url(); ?>assets/bower_components/jquery/dist/jquery.min.js"></script>
+  <script src="<?php echo base_url(); ?>assets/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+</body>
+</html>
