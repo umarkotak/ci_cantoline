ac0db715b520773def7d67140e7c6652af14c1b4
umarkotak <umarkotak@yahoo.co.id>
2017-11-29 08:05:52 +0700

    4 create read update on model

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -1,13 +1,35 @@
 <?php 
 
 class Users extends CI_Controller {
-  public function show(){
+
+  public function show($user_id){
     // loaded on autoload file
     // $this->load->model('user_model');
-    $result = $this->user_model->get_users();
+    $result = $this->user_model->get_users($user_id);
     $data['result'] = $result;    
 
     $this->load->view('admin/users_list_view', $data);
+  }
+
+  public function insert(){
+    $username = "201431291";
+    $password = "201431291";
+
+    $this->user_model->create_users([
+      'username' => $username,
+      'password' => $password
+    ]);
+  }
+
+  public function update(){
+    $id = 3;
+    $username = "201431292";
+    $password = "201431292";
+
+    $this->user_model->update_users([
+      'username' => $username,
+      'password' => $password
+    ], $id);
   }
 
 }
--- application/models/User_model.php
+++ application/models/User_model.php
@@ -1,9 +1,30 @@
 <?php 
 
 class User_model extends CI_Model{  
-  public function get_users(){
+
+  public function get_users($user_id){
+    // $query = $this->db->query("select * from users");
+    $this->db->where([
+      'id' => $user_id
+      // 'username' => $username
+    ]);
+    $this->db->where('id', $user_id);
     $query = $this->db->get('users');
+
+    // result is a helper method
+    // return an array of objects
     return $query->result();
+    // return $query->num_rows();
+    // return $query->num_fields();
+  }
+
+  public function create_users($data){
+    $this->db->insert('users', $data);
+  }
+
+  public function update_users($data, $id){
+    $this->db->where(['id' => $id]);
+    $this->db->update('users', $data);
   }
 }
 
